999f4838675b07bc9a1d1a4f97debf773ff410fb
rachel <rehrlich7@gmail.com>
2016-09-28 11:54:51 -0400

    Made bash script to source smrtanalysis

---
+++ ccs_job_maker.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+#$ -N ccs_job_maker
+#$ -j y
+#$ -cwd
+#$ -pe smp 16
+#$ -R y
+
+SMRT_ROOT="/opt/smrtanalysis"
+
+if [ -e $SMRT_ROOT"/current/etc/setup.sh" ]; then
+    source $SMRT_ROOT"/current/etc/setup.sh"
+    python ccs_job_maker.py "$@"
+else
+    echo "Error:  smrtanalysis not found."
+fi
