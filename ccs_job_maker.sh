3227895ceab2fb6f35b23e9194187ce547eec487
rachel <rehrlich7@gmail.com>
2017-09-06 16:47:29 -0400

    Can call from a different directory

--- ccs_job_maker.sh
+++ ccs_job_maker.sh
@@ -7,12 +7,12 @@
 
 # Author:  Rachel Ehrlich
 
-
 SMRT_ROOT="/opt/smrtanalysis"
 
 if [ -e $SMRT_ROOT"/current/etc/setup.sh" ]; then
     source $SMRT_ROOT"/current/etc/setup.sh"
-    python ccs_job_maker.py "$@"
+    dir=`dirname "$(readlink -f "$0")"`
+    python $dir/ccs_job_maker.py "$@"
 else
     echo "Error:  smrtanalysis not found."
 fi
